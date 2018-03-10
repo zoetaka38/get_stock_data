690c2a9410f3146d7d368d61b3d44365c2be89cb
zoetaka38 <rf.tk.shkp@gmail.com>
2018-03-10 18:13:57 +0900

    change to be able to select if use tor or not

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,31 @@
-FROM python:3
+FROM python:3.6.2
 
-RUN mkdir -p /etc/tor
-ADD brands /brands
-ADD main.py /
-ADD requirements.txt ./
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
 
-RUN pip install -r ./requirements.txt
+# install Dockerize
+ENV DOCKERIZE_VERSION v0.6.0
+
+# install environment dependencies
+RUN apt-get update -yqq \
+  && apt-get install -yqq --no-install-recommends \
+    openssl \
+  && apt-get -q clean
+
+RUN  wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+ && tar -C /usr/local/bin -xzvf dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+ && rm dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+
+ADD ./requirements.txt /usr/src/app/requirements.txt
+
+RUN pip install -r requirements.txt
 
 RUN mkdir -p /data
 ENV SAVE_PATH /data
 
-WORKDIR /
-ADD jholiday.py /
+ADD jholiday.py /usr/src/app
+
+ADD brands /usr/src/app/brands
+ADD main.py /usr/src/app
 
 CMD [ "python", "-u", "./main.py" ]
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import datetime
 from datetime import datetime as dt
 import pandas as pd
 import jsm
-from jholiday import holiday_name
 from jholiday import is_holiday
-import os, sys, csv
+import os
+import sys
+import csv
 from retry import retry
 
 from stem import Signal
@@ -18,33 +19,44 @@ from stem.control import Controller
 import socks
 import socket
 
-import sys
 sys.path.append('./brands')
+from brands import all_brands
 
 def appendstockcsv(code, range_type, start_date, end_date, stockdf):
     q = jsm.Quotes()
     prices = q.get_historical_prices(code, range_type, start_date, end_date)
     for price in prices:
-        stockdf = stockdf.append(pd.Series(price_to_csvl(price), index=['Date','Open','High','Low','Close','Volume', 'Adj Close']), ignore_index=True)
+        stockdf = stockdf.append(pd.Series(price_to_csvl(price), index=['Date','Open','High','Low','Close','Volume', 'AdjClose']), ignore_index=True)
         stockdf = stockdf.drop_duplicates().sort_values(by=["Date"], ascending=False)
     return stockdf
+
+
 def getstockcsv(code, range_type, start_date, end_date):
     q = jsm.Quotes()
     prices = q.get_historical_prices(code, range_type, start_date, end_date)
     stockdf = pd.DataFrame()
     for price in prices:
-        stockdf = stockdf.append(pd.Series(price_to_csvl(price), index=['Date','Open','High','Low','Close','Volume', 'Adj Close']), ignore_index=True)
+        stockdf = stockdf.append(pd.Series(price_to_csvl(price), index=['Date','Open','High','Low','Close','Volume', 'AdjClose']), ignore_index=True)
         stockdf = stockdf.drop_duplicates().sort_values(by=["Date"], ascending=False)
     return stockdf
+
+
 def price_to_csvl(price):
     """株データをCSV形式に変換"""
     return [price.date.strftime('%Y-%m-%d'),
             price.open, price.high, price.low, 
             price.close, price.volume, price._adj_close]
 
+
 SAVE_PATH = os.environ["SAVE_PATH"]
-from brands import all_brands
-args = {"skip":0,}
+USE_TOR = os.environ["USE_TOR"]
+
+type(USE_TOR)
+
+if USE_TOR == True:
+    TOR_REFRESH_TIME = os.environ["TOR_REFRESH_TIME"]
+    type(TOR_REFRESH_TIME)
+args = {"skip": 0, }
 codes = []
 for (i, (code, name, _)) in enumerate(all_brands[args["skip"]:]):
         codes.append(code)
@@ -58,12 +70,14 @@ def init_tor_proxy():
     controller = Controller.from_port(address=torip, port=9051)
     return torip, controller
 
-torip, controller = init_tor_proxy()
 
-controller.authenticate(password=os.environ["TOR_PASSWORD"])
-controller.signal(Signal.NEWNYM)
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, torip, 9050)
-urllib.request.socket.socket = socks.socksocket
+if USE_TOR == True:
+    torip, controller = init_tor_proxy()
+    controller.authenticate(password=os.environ["TOR_PASSWORD"])
+    controller.signal(Signal.NEWNYM)
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, torip, 9050)
+    urllib.request.socket.socket = socks.socksocket
+    tor_used_time = 0
 
 _type = os.environ["TIME_TYPE"]
 i = 0
@@ -73,8 +87,11 @@ if _type == '1':
     for (i, (code, name, _)) in enumerate(all_brands[args["skip"]:]):
         print('{} / {}'.format(i + 1, len(all_brands)), code, name,)
         file = os.path.join(SAVE_PATH, 'YH_JP_{}.csv'.format(code))
-        # change ip address
-        controller.signal(Signal.NEWNYM)
+        # change ip address if tor refresh time has come
+        if USE_TOR == True:
+            if tor_used_time > TOR_REFRESH_TIME:
+                controller.signal(Signal.NEWNYM)
+                tor_used_time = 0
         # すでにデータを取得している場合
         if os.path.isfile(file):
             stockdf = pd.read_csv(file)
@@ -98,8 +115,8 @@ if _type == '1':
                         start_date = start_date + datetime.timedelta(days=1)
                         try:
                             stockdf = appendstockcsv(code, jsm.DAILY, start_date, end_date, stockdf)
-                            stockdf = stockdf.ix[:,['Date','Open','High','Low','Close','Volume', 'Adj Close']]
-                            stockdf.to_csv(file,index=False)
+                            stockdf = stockdf.ix[:,['Date','Open','High','Low','Close','Volume', 'AdjClose']]
+                            stockdf.to_csv(file, index=False)
                             print("got ddata. code: %d" % (int(code)))
                         except Exception as e:
                             print('=== エラー内容 ===',)
@@ -111,8 +128,6 @@ if _type == '1':
                     pass
             # 中途半端にデータを取得してしまっている場合
             else:
-                # change ip address
-                controller.signal(Signal.NEWNYM)
                 # 関数実行日が土日祝の場合、最近の土日祝以外の日にする
                 start_date = datetime.date(1990,1,1)
                 end_date = datetime.date.today()
@@ -120,8 +135,8 @@ if _type == '1':
                     end_date = end_date - datetime.timedelta(days=1)
                 try:
                     stockdf = getstockcsv(code, jsm.DAILY, start_date, end_date)
-                    stockdf = stockdf.ix[:,['Date','Open','High','Low','Close','Volume', 'Adj Close']]
-                    stockdf.to_csv(file,index=False)
+                    stockdf = stockdf.ix[:,['Date','Open','High','Low','Close','Volume', 'AdjClose']]
+                    stockdf.to_csv(file, index=False)
                     print("got ddata. code: %d" % (int(code)))
                 except Exception as e:
                     print('=== エラー内容 ===',)
@@ -130,17 +145,15 @@ if _type == '1':
                     print('e itself:' + str(e),)
         # データを取得したことがない場合
         else:
-            # change ip address
-            controller.signal(Signal.NEWNYM)
             # 関数実行日が土日祝の場合、最近の土日祝以外の日にする
-            start_date = datetime.date(1990,1,1)
+            start_date = datetime.date(1990, 1, 1)
             end_date = datetime.date.today()
             while is_holiday(end_date):
                 end_date = end_date - datetime.timedelta(days=1)
             try:
                 stockdf = getstockcsv(code, jsm.DAILY, start_date, end_date)
-                stockdf = stockdf.ix[:,['Date','Open','High','Low','Close','Volume', 'Adj Close']]
-                stockdf.to_csv(file,index=False)
+                stockdf = stockdf.ix[:,['Date','Open','High','Low','Close','Volume', 'AdjClose']]
+                stockdf.to_csv(file, index=False)
                 print("got ddata. code: %d" % (int(code)))
             except Exception as e:
                 print('=== エラー内容 ===',)
